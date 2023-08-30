f820dc6d117f919149d2377b67592264f27e91c8
Houston Culpepper <howacul@gmail.com>
2023-08-30 14:33:36 -0400

    update webpack.config.js to properly render icons and fixed service worker not loading by adding missing module import

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,6 +25,8 @@ module.exports = () => {
         swDest: "src-sw.js",
       }),
       new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
         name: "Just Another Text Editor",
         short_name: "JATE",
         description:
